0aa2c79f871a7be9ada1ac25cf95bde027ac417a
“MARIA <MDFORTINO@GMAIL.COM>
2019-01-08 16:29:02 -0500

    added image and containers to html

--- script.js
+++ script.js
@@ -4,22 +4,36 @@
 //--> store user answer
 //--> check if correct
 //--> track score (how many user answers are correct)
-//method to countdown: setInterval()
-//--> reset and show next question after selection made and/or after 30 seconds is up
-//method to track and display time remaining: getTime(), Math.floor
+//--> how many questions left 
+
+//--> reset and show next question after select next button  and/or after 30 seconds is up
+
 
 //create array of all questions
 let questions = [{
-                question: "What is the newest national park?",
-                choices: ["Congaree", "Big Bend", "Mount Rainier", "Great Sand Dunes"],
-                correctAnswer: "Great Sand Dunes" }
-                        
+        question: "What is the newest national park?",
+        choices: ["Congaree", "Big Bend", "Mount Rainier", "Great Sand Dunes"],
+        correctAnswer: "Great Sand Dunes"
+    },
+    {
+        question: "What is the most visited park in the National Park System?",
+        choices: ["Acadia", "Grand Canyon", "Great Smoky Mountains", "Yellowstone"],
+        correctAnswer: "Great Smoky Mountains"
+    },
+    {
+        question: "Which of the following exists within the boundaries of a national park?",
+        choices: ["The highest point in North America", "The longest cave system in the world", "The deepest lake in the U.S.", "All of the above" ],
+        correctAnswer: "All of the above"
+    }
 ]
 
-let answers 
-let results 
+let answers
+let results
 
-let seconds 
+
+//method to countdown: setInterval()
+//method to track and display time remaining: getTime(), Math.floor
+let seconds
 
 
 //will need events for answer buttons: 
